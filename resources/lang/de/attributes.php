5be1f38f067d61bd31a65229acf0bc1e8df2e807
Misaf <misaf.1990@gmail.com>
2026-05-13 13:08:59 +0330

    feat: add German translations

---
+++ resources/lang/de/attributes.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return  [
+    'created_at'       => 'Erstellt am',
+    'id'               => 'ID',
+    'name'             => 'Name',
+    'slug'             => 'Slug',
+    'slug_helper_text' => 'Eine eindeutige Kennung für die URL. Leer lassen, um sie automatisch aus dem Namen zu generieren.',
+    'tenant_id'        => 'Mandant',
+    'type'             => 'Typ',
+    'updated_at'       => 'Aktualisiert am',
+];
---
+++ resources/lang/de/navigation.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return  [
+    'tagger'            => 'Tagger',
+    'tagger_management' => 'Tag-Verwaltung',
+];
