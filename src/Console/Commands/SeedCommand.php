d9a62cc7c362659fe76dbc8307ad724a38ed3911
misaf <misaf.1990@gmail.com>
2026-09-17 19:49:00 +0000

    test(store): cover the storefront:lifecycle command

--- src/Console/Commands/SeedCommand.php
+++ src/Console/Commands/SeedCommand.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace Misaf\VendraTagger\Console\Commands;
 
 use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
 use Misaf\VendraSupport\Tenancy\Console\Commands\TenantSeedCommand;
 use Misaf\VendraTagger\Database\Seeders\PermissionPolicySeeder;
 use Misaf\VendraTagger\TaggerPlugin;
 
 #[Description('Seed tagger module data for a tenant')]
+#[Signature(self::MODULE_NAME.':seed
+        {tenant? : Tenant ID or slug to seed tagger data for}
+        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies}')]
 final class SeedCommand extends TenantSeedCommand
 {
     protected const string MODULE_NAME = TaggerPlugin::ID;
-
-    protected $signature = self::MODULE_NAME.':seed
-        {tenant? : Tenant ID or slug to seed tagger data for}
-        {seeders?* : Seeder keys to run. Use "all" or one or more of: permission-policies}';
 
     /**
      * @return array<string, class-string>
